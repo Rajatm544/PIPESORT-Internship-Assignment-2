e58f85757a61f93832db13830ffc26b0f49de586
Rajatm544 <rajatm544@gmail.com>
2020-05-10 15:52:22 +0530

    add build

---
+++ client-side/build/precache-manifest.b69020e3f7ddeb6bd31e24ee7149849f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e2a0f57c0a3a89e91faa43903b8089a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f48360170a6305f7608c",
+    "url": "/static/css/main.66e07ecf.chunk.css"
+  },
+  {
+    "revision": "da9e42b0df36dccdc612",
+    "url": "/static/js/2.ecc292b1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ecc292b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f48360170a6305f7608c",
+    "url": "/static/js/main.d6ae2ac8.chunk.js"
+  },
+  {
+    "revision": "b5eff3a7135ae2f69b1a",
+    "url": "/static/js/runtime-main.421d6c45.js"
+  }
+]);
