6bdc318ed22df5db7cb21a3a3d9e2ae4b04dcdec
Rajatm544 <rajatm544@gmail.com>
2020-05-10 18:35:49 +0530

    add config var for heroku

---
+++ client-side/build/precache-manifest.e23d7fc999a20c4dcb6e7fbbf860f034.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "438e13187b5c7cda6faed78f8c46f651",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65a86e3a9408b836bcfb",
+    "url": "/static/css/main.66e07ecf.chunk.css"
+  },
+  {
+    "revision": "da9e42b0df36dccdc612",
+    "url": "/static/js/2.ecc292b1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ecc292b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65a86e3a9408b836bcfb",
+    "url": "/static/js/main.9f220a16.chunk.js"
+  },
+  {
+    "revision": "b5eff3a7135ae2f69b1a",
+    "url": "/static/js/runtime-main.421d6c45.js"
+  }
+]);
